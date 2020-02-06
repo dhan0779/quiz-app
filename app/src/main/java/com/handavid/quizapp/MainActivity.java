20e82980d32b7ca0bd390ba6c9f6c32f8e684f0d
David Han <david2003han@gmail.com>
2020-02-06 09:42:18 -0500

    added 4 buttons

--- app/src/main/java/com/handavid/quizapp/MainActivity.java
+++ app/src/main/java/com/handavid/quizapp/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button submitButton, colorButton;
+    Button ansButton1, ansButton2,ansButton3,ansButton4;
     EditText responseText;
     EditText response2Text;
     TextView displayText;
@@ -19,26 +19,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        submitButton=findViewById(R.id.clickButton);
-        colorButton=findViewById(R.id.clickButton2);
+        ansButton1=findViewById(R.id.clickButton);
+        ansButton2=findViewById(R.id.clickButton2);
+        ansButton3=findViewById(R.id.clickButton3);
+        ansButton4=findViewById(R.id.clickButton4);
         responseText=findViewById(R.id.responseEditText);
         response2Text=findViewById(R.id.response2EditText);
         displayText=findViewById(R.id.textBox);
-        submitButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                System.out.println("Hello mom");
-                Log.i("testButton","Hi dad! "+responseText.getText().toString());
-                displayText.setText("Hi "+ responseText.getText().toString());
-            }
-        });
-        colorButton.setOnClickListener(new View.OnClickListener(){
+        ansButton1.setOnClickListener(new View.OnClickListener(){
             //@Override
             int a=0;
             public void onClick(View view){
                 a=a+1;
                 displayText.setText("You clicked this button "+a+" times");
                 view.setBackgroundColor(Color.GREEN);
+            }
+        });
+        ansButton2.setOnClickListener(new View.OnClickListener(){
+            //@Override
+            int b=0;
+            public void onClick(View view){
+                b=b+1;
+                displayText.setText("You clicked this button "+b+" times");
+                view.setBackgroundColor(Color.RED);
+            }
+        });
+        ansButton3.setOnClickListener(new View.OnClickListener(){
+            //@Override
+            int c=0;
+            public void onClick(View view){
+                c=c+1;
+                displayText.setText("You clicked this button "+c+" times");
+                view.setBackgroundColor(Color.BLUE);
+            }
+        });
+        ansButton4.setOnClickListener(new View.OnClickListener(){
+            //@Override
+            int d=0;
+            public void onClick(View view){
+                d=d+1;
+                displayText.setText("You clicked this button "+d+" times");
+                view.setBackgroundColor(Color.YELLOW);
             }
         });
         responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
