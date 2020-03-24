9278ba3ecc3d1999c553ff39d9cca1ee9a10d5ec
dhan0779 <david2003han@gmail.com>
2020-03-24 18:17:17 -0400

    Lab06d functionality of changing activity works now (arrayOutofBounds) and used Gson and SharedPreferences

---
+++ app/src/main/java/com/handavid/quizapp/Leaderboard_Stat.java
@@ -0,0 +1,14 @@
+package com.handavid.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Leaderboard_Stat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard__stat);
+    }
+}
